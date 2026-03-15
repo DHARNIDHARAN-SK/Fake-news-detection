ef36e01e998b9ae3439d09fd86a4f5cd0962b024
DHARANIDHARAN-SK <karnan284858@gmail.com>
2026-03-15 10:44:50 +0000

    updated routing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def predict():
     return jsonify({"prediction": result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
